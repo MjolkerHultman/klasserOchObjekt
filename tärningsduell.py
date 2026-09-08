a43ddb74b257ae034b308b2787a034bd833e8a40
MjolkerHultman <melker.hultman@elev.ga.ntig.se>
2026-09-08 12:27:00 +0200

    new approach to scoring

--- tärningsduell.py
+++ tärningsduell.py
@@ -1,20 +1,49 @@
 #Comparison method that goes from 6 to 1 in descending order and checks which player is first. Maybe more scalable, therefore somewhat more interesting to muck about with
-
-from random import random
+#nevermind that ^. Make a dict(?) with the saved casts and a name, then sort them by their casts value, then find the name and credit the winning player
+import random
 
 class Player():
-    def __init__(self, name, ):
+    def __init__(self, name):
         self.name = name
         self.score = 0
+        self.cast = 0
 
-player_count = int(input("How many players are there?"))
+    def throw(self):
+        self.cast = random.randrange(1,6)
+
+    def score(self):
+        self.score += 1
+
+playerCount = int(input("How many players are there?"))
 players = []
-for i in range(player_count):
-    temporaryObject = Player(input("What is the name of player number " + str(i + 1) + "?"))
+for i in range(playerCount):
+    temporaryObject = Player(input(f"What is the name of player number {str(i + 1)} ?"))
     players.append(temporaryObject)
 
-for i in range(player_count):
-    print(players[i].name)
+play = True
+
+while play:
+    for player in players:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for testing the content of players
+#for i in range(player_count):
+    #print(players[i].name)
 
 #player = Player("HWECFDGWEHFG")
 #players = []
